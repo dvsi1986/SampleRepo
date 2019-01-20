0d42a5a63ba766eb854f7cc013140bac4220b7c9
Siva <dvsi1986@gmail.com>
2019-01-20 12:26:11 -0600

    Test.java has added to the Master

--- Test.java
+++ Test.java
@@ -7,7 +7,7 @@ import java.time.format.DateTimeFormatter;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void   main(String[] args) {
 		String now = "2016-11-09 10:30:00";
 
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
